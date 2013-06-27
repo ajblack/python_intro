8b3e93989e83aaa67ced572b4f0949b75fefa38e
Austin Blackman <austin.blackman@gmail.com>
2013-06-26 22:47:55 -0400

    starting a datatype for tableau-set

--- kata1.py
+++ kata1.py
@@ -28,6 +28,11 @@ class Card(object):
   elif self.rank == 13:
     print "king of ",self.suit
 
+class Tableau(object):
+  def __init__(self, isEmpty, tableau_stack):
+    self.isEmpty=isEmpty
+    self.isEmpty=False
+    
 
  
 class The_Deck(object):
@@ -48,47 +53,55 @@ class The_Game(object):
 
   def recite_game_status(self):
     print "here is the status of your game..."
-    first_c=tableau_one.pop(0)
-    print "your first tableau has ", len(tableau_one), " cards in it and there is a ",
-    first_c.get_card_info(), 
-    print"on top"
-    tableau_one.insert(0,first_c)
+    if len(tableau_one)>0:
+      first_c=tableau_one[0]
+      print "your first tableau has ", len(tableau_one), " cards in it and the top card is a ",
+      first_c.get_card_info()
+    else:
+      print "tableau one is empty"
 
-    second_c=tableau_two.pop(0)
-    print "your second tableau has ", len(tableau_two), " cards in it and there is a ",
-    second_c.get_card_info(), 
-    print"on top"
-    tableau_two.insert(0,second_c)
+    if len(tableau_two)>0:
+      second_c=tableau_two[0]
+      print "your second tableau has ", len(tableau_two), " cards in it and the top card is a ",
+      second_c.get_card_info()
+    else:
+      print "tableau two is empty"
 
-    third_c=tableau_three.pop(0)
-    print "your third tableau has ", len(tableau_three), " cards in it and there is a ",
-    third_c.get_card_info(), 
-    print"on top"
-    tableau_one.insert(0,first_c)
+    if len(tableau_three)>0:
+      third_c=tableau_three[0]
+      print "your third tableau has ", len(tableau_three), " cards in it and the top card is a ",
+      third_c.get_card_info()
+    else:
+      print "tableau three is empty"
+      
 
-    fourth_c=tableau_four.pop(0)
-    print "your fourth tableau has ", len(tableau_four), " cards in it and there is a ",
-    fourth_c.get_card_info(), 
-    print"on top"
-    tableau_four.insert(0,fourth_c)
+    if len(tableau_four)>0:
+      fourth_c=tableau_four[0]
+      print "your fourth tableau has ", len(tableau_four), " cards in it and the top card is a ",
+      fourth_c.get_card_info()
+    else:
+      print "tableau four is empty"
 
-    fifth_c=tableau_five.pop(0)
-    print "your fifth tableau has ", len(tableau_five), " cards in it and there is a ",
-    fifth_c.get_card_info(), 
-    print "on top"
-    tableau_one.insert(0,fifth_c)
+    if len(tableau_five)>0:
+      fifth_c=tableau_five[0]
+      print "your fifth tableau has ", len(tableau_five), " cards in it and the top card is a ",
+      fifth_c.get_card_info()
+    else:
+      print "tableau five is empty"
 
-    sixth_c=tableau_six.pop(0)
-    print "you sixth tableau has ", len(tableau_six), " cards in it and there is a ",
-    sixth_c.get_card_info(), 
-    print "on top"
-    tableau_one.insert(0,sixth_c)
+    if len(tableau_six)>0:
+      sixth_c=tableau_six[0]
+      print "you sixth tableau has ", len(tableau_six), " cards in it and the top card is a ",
+      sixth_c.get_card_info()
+    else:
+      print "tableau six is empty"
 
-    seventh_c=tableau_seven.pop(0)
-    print "you seventh tableau has ", len(tableau_seven), " cards in it and there is a ",
-    seventh_c.get_card_info(), 
-    print" on top"
-    tableau_seven.insert(0,seventh_c)
+    if len(tableau_seven)>0:
+      seventh_c=tableau_seven[0]
+      print "you seventh tableau has ", len(tableau_seven), " cards in it and the top card is a ",
+      seventh_c.get_card_info()
+    else:
+      print "tableau seven is empty"
 
   def deal_initial_tableaus(self):
     tableau_one.insert(0,self.deal_single_card())
@@ -98,8 +111,6 @@ class The_Game(object):
     tableau_five.insert(0,self.deal_single_card())
     tableau_six.insert(0,self.deal_single_card())
     tableau_seven.insert(0,self.deal_single_card())
-
-    
 
     tableau_two.insert(0,self.deal_single_card())
     tableau_three.insert(0,self.deal_single_card())
